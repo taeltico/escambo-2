7166ed8bd13d3249c4d52553b0162940c9fafd1d
taeltico <rafaelwvs16@gmaail.com>
2022-05-06 19:57:49 -0300

    api

--- api/tempo.js
+++ api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request,response) {
+async function tempo(request,response) {
+    const  apiSecrret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
+    
+    const subscribersResponse = await fetch("");
+    const subscribersResponsejson = await subscribersResponse.json();
+    const inscritos = subscribersResponsejson.total_subscribers;
 
     response.json({
         date: dynamicDate.toGMTSting()
+        inscritos: inscritos
     })
 }
 
